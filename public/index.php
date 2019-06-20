58f7bc3e245d760ca1937a3e8e7e72d923245cce
stavarengo <faelsta@gmail.com>
2019-06-20 18:14:21 -0300

    First commit

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+chdir(dirname(__DIR__));
+
+if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    $msg = 'Did you forgot to run `composer install`?' . PHP_EOL . 'Unable to load the "./vendor/autoload.php".';
+    http_response_code(500);
+    echo "<pre>$msg</pre>";
+    throw new RuntimeException($msg);
+}
+require __DIR__ . '/../vendor/autoload.php';
+
+/**
+ * I'm using a self-called anonymous function to create its own scope and keep the the variables created here away from
+ * the global scope.
+ */
+(function () {
+    http_response_code(200);
+    echo 'It works!';
+})();
